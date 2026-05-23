1d74087e7d67bf86d95648d6ef063b4c88a0f96c
Shishir Gupta <shishirgupta568@gmail.com>
2026-05-23 10:58:02 +0530

    initial comm

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import {store} from "./store/index.js";
 import { register } from './serviceWorkerRegistration.js';
@@ -10,10 +10,10 @@ import { register } from './serviceWorkerRegistration.js';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
      <App/>
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
 )
 register();
